06ac2c320d7f66feb602fdee6293778b48374865
Jeremy <jsheridanwells@gmail.com>
2018-02-15 12:56:09 -0600

    added exception filter

---
+++ Landon.Api/Models/ApiError.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using Newtonsoft.Json;
+
+namespace Landon.Api.Models
+{
+    public class ApiError
+    {
+        public string Message { get; set; }
+        public string Detail { get; set; }
+
+        [JsonProperty(
+            NullValueHandling = NullValueHandling.Ignore,
+            DefaultValueHandling = DefaultValueHandling.Ignore
+        )]
+        [DefaultValue("")]
+        public string StackTrace { get; set; }
+    }
+}
--- Landon.Api/Startup.cs
+++ Landon.Api/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Landon.Api.Filters;
 using Landon.Api.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -28,6 +29,7 @@ namespace Landon.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc(opt => {
+                opt.Filters.Add(typeof(JsonExceptionFilter));
                 var jsonFormatter = opt.OutputFormatters.OfType<JsonOutputFormatter>().Single();
                 opt.OutputFormatters.Remove(jsonFormatter);
                 opt.OutputFormatters.Add(new IonOutputFormatter(jsonFormatter));
