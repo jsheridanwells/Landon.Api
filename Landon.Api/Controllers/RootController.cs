00909d8be25d9f264b3c25df2b72499473b85529
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2018-02-15 11:53:17 -0600

    added api versioning in headers

--- Landon.Api/Controllers/RootController.cs
+++ Landon.Api/Controllers/RootController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Landon.Api.Controllers
 {
     [Route("/")]
+    [ApiVersion("1.0")]
     public class RootController : Controller
     {
         [HttpGet(Name = nameof(GetRoot))]
